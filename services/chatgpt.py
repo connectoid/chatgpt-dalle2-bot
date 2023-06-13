ecb1aa6deca4d7e133440edff0a00ae6768b7290
Alexander Beley <beleysasha@gmail.com>
2023-06-14 09:57:11 +1200

    смена движка на chatgpt 3.5 turbo

--- services/chatgpt.py
+++ services/chatgpt.py
@@ -4,19 +4,34 @@ from config_data.config import Config, load_config
 
 config: Config = load_config()
 openai.api_key = config.open_ai.key
-engine = 'text-davinci-003'
-#engine = 'gpt-3.5-turbo'
+#engine = 'text-davinci-003'
+engine = 'gpt-3.5-turbo'
+role = 'user'
+messages = [
+    {"role": "system",
+     "content" : "You are ChatGPT, "\
+     "a large language model trained by OpenAI. Answer as concisely as possible.\n"\
+     "Knowledge cutoff: 2022-09-01\nCurrent date: 2023-03-02"},
+
+]
+
+
+def update(messages, role, content):
+    messages.append({"role": role, "content": content})
+    return messages
+
 
 def get_answer(prompt):
     try:
-        completion = openai.Completion.create(
-            engine=engine,
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=1000
+        update(messages, "user", prompt)
+        completion = openai.ChatCompletion.create(
+            model=engine,
+            messages=messages,
         )
-        return completion.choices[0]['text']
+        print(completion)
+        return completion['choices'][0]['message']['content']
 
     except TypeError as error:
         print('Ошибка: ', error)
         return None
+
